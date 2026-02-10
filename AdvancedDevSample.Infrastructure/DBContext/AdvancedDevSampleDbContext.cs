b6e7dac5cb222688391401156b15ac2afda0343a
DJOKOUO Kelvine <djokouokelvine@gmail.com>
2026-02-10 13:57:44 +0100

    Initialize project

--- AdvancedDevSample.Infrastructure/DBContext/AdvancedDevSampleDbContext.cs
+++ AdvancedDevSample.Infrastructure/DBContext/AdvancedDevSampleDbContext.cs
@@ -8,7 +8,7 @@ namespace AdvancedDevSample.Infrastructure.DBContext
         public AdvancedDevSampleDbContext(DbContextOptions<AdvancedDevSampleDbContext> options) : base(options) { }
 
 
-        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Product> Products => Set<Product>(); 
         //public DbSet<Customer> Customers => Set<Customer>();
         //public DbSet<Order> Orders => Set<Order>();
         //public DbSet<OrderLine> OrderLines => Set<OrderLine>();
