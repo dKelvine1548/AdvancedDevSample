3fc3975dd8ad00b88a43793c97ec7a3a0c5e84b0
DJOKOUO Kelvine <djokouokelvine@gmail.com>
2026-02-14 04:12:16 +0100

    fix: correct service product file

--- AdvancedDevSample.Api/Controllers/ProductController.cs
+++ AdvancedDevSample.Api/Controllers/ProductController.cs
@@ -13,13 +13,16 @@ namespace AdvancedDevSample.Api.Controllers
         private readonly ProductService _productService;
 
         public ProductController(ProductService productService) { 
+
             _productService = productService; 
+
         }
 
         [HttpPost]
         public IActionResult CreateProduct(CreateProductDto dto)
         {
             var product = _productService.Create(dto); 
+
             return Ok(product);
         }
 
